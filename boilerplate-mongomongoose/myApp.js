3dc4a08c370b3e51298d69eedc984912b0ac968d
Kam . <rkgvkamal2003@gmail.com>
2025-04-13 18:46:50 +0000

    mongomongoose 7/12 done

--- boilerplate-mongomongoose/myApp.js
+++ boilerplate-mongomongoose/myApp.js
@@ -35,26 +35,31 @@ const createAndSavePerson = (done) => {
   // done(null /*, data*/);
 };
 
+//maybe do this:
+// Person.create(data) just lev this
+
 
 // creating multiple records at once... 
 // its sort of cofucious. take care 
 // Model.create() takes 2 args; arrayOfPeople: array of records callback: the usual (err,data){if (err) return done(err) else return done(null,data)}
-let arrayOfPeople = 
-[{
-  name: "sh",
-  age: 22,
-  favoriteFoods: ["none"]
-},
-{
-  name: "kam",
-  age: 21,
-  favoriteFoods: ["some"]
-},
-{
-  name: "man",
-  age: 21,
-  favoriteFoods: ["maybe"]
-}];
+
+
+// let arrayOfPeople = 
+// [{
+//   name: "sh",
+//   age: 22,
+//   favoriteFoods: ["none"]
+// },
+// {
+//   name: "kam",
+//   age: 21,
+//   favoriteFoods: ["some"]
+// },
+// {
+//   name: "man",
+//   age: 21,
+//   favoriteFoods: ["maybe"]
+// }];
 const createManyPeople = (arrayOfPeople, done) => {
   Person.create(arrayOfPeople,(err,data)=>{
     if (err) return done(err);
@@ -65,22 +70,42 @@ const createManyPeople = (arrayOfPeople, done) => {
 };
 
 const findPeopleByName = (personName, done) => {
-  
-  done(null /*, data*/);
+  Person.find({name: personName},(err,data)=>{
+    if (err) return console.log(done(err));
+    done(null, data);
+});
+  // done(null /*, data*/);
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({favoriteFoods: food },(err,data)=> {
+    if (err) return done(err);
+    return done(null,data)
+  })
+  // done(null /*, data*/);
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId,(err,data)=> {
+    if (err) return done(err);
+    return done(null,data)
+  })
+  // done(null /*, data*/);
 };
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
+  Person.findById(personId,(err,data)=> {
+    if (err) return done(err);
+    console.log("@@",data);
+    data.favoriteFoods.push(foodToAdd);
+    data.save((err,data)=> {
+      if (err) return done(err);
+      return done(null,data);
+  });
+})
 
-  done(null /*, data*/);
+  // done(null /*, data*/);
 };
 
 const findAndUpdate = (personName, done) => {
